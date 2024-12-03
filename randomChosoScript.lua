6fd28bbf712bd6bf608946e8ad5ea74c517d7647
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2024-12-03 12:46:53 +0300

    Update randomChosoScript.lua

--- randomChosoScript.lua
+++ randomChosoScript.lua
@@ -1,5 +1,16 @@
 local args1 = { [1] = "Choso" }
 local table1 = {}
+local succes, result pcall(function() 
+    --[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+local args = {
+    [1] = "SetMoveset",
+    [2] = "TheRandomgojo"
+}
+    end)
+
+game:GetService("ReplicatedStorage").Remotes.Comunicator.Set:FireServer(unpack(args))
 game:GetService("ReplicatedStorage"):WaitForChild("Knit"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("JoinService"):WaitForChild("RE"):WaitForChild("Change"):FireServer(unpack(args))
 local function startLoop(skillName, serviceName, scriptTable, extraArgs)
     spawn(function()
